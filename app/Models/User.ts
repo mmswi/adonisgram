51e0ccce5b6dd2cfc32d154decf62512c25f3538
Mihai Marinescu <marinescu.mihai88@gmail.com>
2021-08-07 13:54:57 +0300

    feat: hashes password in beforeSave hook

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, beforeSave, column } from '@ioc:Adonis/Lucid/Orm'
+import Hash from '@ioc:Adonis/Core/Hash'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
@@ -13,6 +14,13 @@ export default class User extends BaseModel {
 
   @column()
   public password: string
+
+  @beforeSave()
+  public static async hashPassword(user: User) {
+    if (user.$dirty.password) {
+      user.password = await Hash.make(user.password)
+    }
+  }
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
