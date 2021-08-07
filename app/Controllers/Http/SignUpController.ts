a7978413452034624e7749ca8d0da5b63d0aa97d
Mihai Marinescu <marinescu.mihai88@gmail.com>
2021-08-07 11:18:23 +0300

    feat: adds form validation

--- app/Controllers/Http/SignUpController.ts
+++ app/Controllers/Http/SignUpController.ts
@@ -1,7 +1,25 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import { schema, rules } from '@ioc:Adonis/Core/Validator';
+
 
 export default class SignUpController {
-    public async index({request}: HttpContextContract) {
+    public async index({ request }: HttpContextContract) {
+        const req = await request.validate({
+            schema: schema.create({
+                name: schema.string(),
+                email: schema.string({}, [
+                    rules.email(),
+                ]),
+                password: schema.string({}, [
+                    rules.confirmed('password_confirm')
+                ]),
+                'password_confirm': schema.string()
+            }),
+            messages: {
+                required: 'The {{ field }} is required to create a new account',
+            }
+        })
+        console.log(req);
         return request.all();
     }
 }
