e6f8133fc9fb89993b7343d65b50c7704f8e9d50
Mihai Marinescu <marinescu.mihai88@gmail.com>
2021-08-07 13:41:04 +0300

    feat: saves user in the db

--- app/Controllers/Http/SignUpController.ts
+++ app/Controllers/Http/SignUpController.ts
@@ -1,9 +1,10 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import { schema, rules } from '@ioc:Adonis/Core/Validator';
+import User from 'App/Models/User';
 
 
 export default class SignUpController {
-    public async index({ request }: HttpContextContract) {
+    public async index({ request, response }: HttpContextContract) {
         const req = await request.validate({
             schema: schema.create({
                 name: schema.string(),
@@ -19,7 +20,12 @@ export default class SignUpController {
                 required: 'The {{ field }} is required to create a new account',
             }
         })
-        console.log(req);
-        return request.all();
+        
+        const user = new User();
+        user.name = req.name;
+        user.email = req.email;
+        user.password = req.password;
+        user.save();
+        return response.redirect('/');
     }
 }
