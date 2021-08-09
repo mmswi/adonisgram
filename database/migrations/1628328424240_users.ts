0c28e126500c13bd76c607c55842c23b68430e2a
Mihai Marinescu <marinescu.mihai88@gmail.com>
2021-08-09 12:48:51 +0300

    feat: migration rollback and run again

--- database/migrations/1628328424240_users.ts
+++ database/migrations/1628328424240_users.ts
@@ -6,8 +6,10 @@ export default class Users extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
+      table.string('name')
       table.string('email')
       table.string('password')
+      table.dateTime('email_verified_at').nullable()
       table.timestamps()
     })
   }
