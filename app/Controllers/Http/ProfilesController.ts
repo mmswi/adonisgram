480521e7c8721e9c93b7c0292cfd21439d94247b
Mihai Marinescu <marinescu.mihai88@gmail.com>
2021-08-10 09:04:31 +0300

    feat: shows 404 for user not found

--- app/Controllers/Http/ProfilesController.ts
+++ app/Controllers/Http/ProfilesController.ts
@@ -1,7 +1,15 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import User from 'App/Models/User';
 
 export default class ProfilesController {
-    public async index({ view }: HttpContextContract) {
-        return view.render('user/profile')
+    public async index({ view, params }: HttpContextContract) {
+        const username = params.username
+        try {
+            await User.findByOrFail('username', username);
+            return view.render('user/profile')
+        } catch (e) {
+            return view.render('errors/not-found')
+        }
+        
     }
 }
